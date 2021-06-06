5fa2b5d065187c86a7b9b8125c252d335a632398
olenmg <o.olenmg@gmail.com>
2021-06-06 14:55:42 +0900

    <chore> modify script

--- baseline/scripts/optuna_search.sh
+++ baseline/scripts/optuna_search.sh
@@ -1,4 +1,4 @@
 python optuna_search.py --n_trials 100 \
-                        --study_name 0604_study \
+                        --study_name 0606_study \
                         --save_model True \
-                        --base /opt/ml/input/config/optuna_config/0604_study.yaml
+                        --base /opt/ml/input/config/optuna_config/0606_study.yaml
