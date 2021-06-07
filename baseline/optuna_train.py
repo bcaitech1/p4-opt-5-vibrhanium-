d77dc07eb2a7fba911822fde3d75dafc4e93fc9a
jaegyeongbyun <worud1208@gmail.com>
2021-06-07 11:53:52 +0900

    <fix> best.pt 저장 경로 변경 - model, hyperparamter가 저장된 곳에 함께 저장

--- baseline/optuna_train.py
+++ baseline/optuna_train.py
@@ -117,8 +117,8 @@ if __name__ == "__main__":
     hyperparam_config = read_yaml(cfg=args.hyperparam)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    log_dir = os.path.dirname(args.weight)
     
+    log_dir = os.path.dirname(args.model)
     os.makedirs(log_dir, exist_ok=True)
 
     test_loss, test_f1, test_acc = train(
