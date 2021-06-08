356533bc21a53ee44e3f070a9f20829911ec01db
jaegyeongbyun <worud1208@gmail.com>
2021-06-08 11:26:42 +0900

    <fix> Update optuna_train.py - --model 인자로 받은 경로를 사용하므로, os.makedirs 삭제

--- baseline/optuna_train.py
+++ baseline/optuna_train.py
@@ -122,7 +122,6 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
     log_dir = os.path.dirname(args.model)
-    os.makedirs(log_dir, exist_ok=True)
 
     test_loss, test_f1, test_acc = train(
         model_config=model_config,
