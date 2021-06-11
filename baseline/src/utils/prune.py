1811214fbb856eb6f6f56f35b68ce72a295924c9
hwan17 <kimmh6017@gmail.com>
2021-06-11 20:38:12 +0900

    add_prune

---
+++ baseline/src/utils/prune.py
@@ -0,0 +1,33 @@
+import torch.nn.utils.prune as prune
+import torch.nn.functional as F
+import torch
+
+def model_prune(
+    model: nn.Module,
+    
+    conv_prun_norm: int = 1,
+    conv_prun_rate: float = 0.2,
+    linear_prun_norm: int = 1,
+    linear_prun_rate: float = 0.4,
+    structured: bool = False
+):
+    
+    for name, module in model.named_modules():
+        if isinstance(module, torch.nn.modules.conv.Conv2d):
+            if structured:
+                prune.ln_structured(module, name='weight', amount=conv_prun_rate, n=conv_prun_norm, dim=0)
+            else:
+                prune.l2_unstructured(module, name='weight', amount=conv_prun_rate)
+            prune.remove(module,'weight')
+
+        elif isinstance(module, torch.nn.modules.linear.Linear):
+            if structured:
+                prune.ln_structured(module, name='weight', amount=linear_prun_rate, n=conv_prun_norm, dim=0)
+            else:
+                prune.l2_unstructured(module, name='weight', amount=linear_prun_rate)
+            prune.remove(module,'weight')
+    
+    print("-"*10+'prun 적용 모듈'+"-"*10)
+    print(dict(model.named_buffers()).keys())
+    
+    return model
