cb506efef5f48a60c0cc391db853dadee205203c
JaegyeongByun <71882533+JaegyeongByun@users.noreply.github.com>
2021-06-14 16:50:47 +0900

    Update prune.py

--- baseline/src/utils/prune.py
+++ baseline/src/utils/prune.py
@@ -152,6 +152,44 @@ def set_pruned_channels(pruned_module, name, new_pruned_channels=None):
     return pruned_module
 
 
+def model_prune(
+    model: nn.Module,
+
+    conv_prun_norm: int = 1,
+    conv_prun_rate: float = 0.2,
+    linear_prun_norm: int = 1,
+    linear_prun_rate: float = 0.4,
+    structured: bool = False
+):
+
+    for name, module in model.named_modules():
+        if isinstance(module, torch.nn.modules.conv.Conv2d):
+            if structured:
+                prune.ln_structured(module, name='weight', amount=conv_prun_rate, n=conv_prun_norm, dim=0)
+            else:
+                if conv_prun_norm == 1:
+                    prune.l1_unstructured(module, name='weight', amount=conv_prun_rate)
+                else:
+                    prune.l2_unstructured(module, name='weight', amount=conv_prun_rate)
+            prune.remove(module,'weight')
+
+        elif isinstance(module, torch.nn.modules.linear.Linear):
+            if structured:
+                prune.ln_structured(module, name='weight', amount=linear_prun_rate, n=conv_prun_norm, dim=0)
+            else:
+                if linear_prun_norm == 1:
+                    prune.l1_unstructured(module, name='weight', amount=linear_prun_rate)
+                else:
+                    prune.l2_unstructured(module, name='weight', amount=linear_prun_rate)
+
+            prune.remove(module,'weight')
+
+    print("-"*10+'prun 적용 모듈'+"-"*10)
+    print(dict(model.named_buffers()).keys())
+
+    return model
+
+
 @torch.no_grad()
 def model_structured_prune_for_resnet34(
     model: nn.Module,
