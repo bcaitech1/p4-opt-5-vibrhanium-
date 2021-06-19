b425ba75ea7baafbaae7b57fba14806c10050761
jaegyeongbyun <worud1208@gmail.com>
2021-06-19 13:25:14 +0900

    <feat> train_pretrained.sh

---
+++ baseline/scripts/train_pretrained.sh
@@ -0,0 +1,3 @@
+python train_pretrained.py  --model Resnet34  \
+                            --data /opt/ml/p4-opt-5-vibrhanium-/baseline/configs/data/kaggle.yaml  \
+                            --num_classes 4
--- baseline/train_pretrained.py
+++ baseline/train_pretrained.py
@@ -7,92 +7,94 @@ import argparse
 from datetime import datetime
 import os
 import yaml
-from typing import Any, Dict, Tuple, Union
-from importlib import import_module
+from typing import Any, Dict, Tuple
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import torchvision.models
+import torch.nn.functional as F
 
 from src.dataloader import create_dataloader
 from src.loss import CustomCriterion
-from src.trainer_pretrained_wandb import TorchTrainer
+
+from src.trainer import TorchTrainer
+
 from src.utils.common import get_label_counts, read_yaml
 from src.utils.macs import calc_macs
-from src.utils.torch_utils import check_runtime, model_info
+from src.utils.prune import (
+    model_structured_prune_for_resnet34,
+    model_structured_prune_for_shufflenet_v2,
+)
 
-import timm
 
-# model_list = [
-#     'alexnet', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn',
-#     'vgg16', 'vgg19', 'vgg19_bn', 'resnet18', 'resnet34',
-#     'resnet50', 'resnet101', 'resnet152', 'squeezenet1_0', 'squeezenet1_1',
-#     'densenet121', 'densenet169', 'densenet161', 'densenet201', 'inception_v3',
-#     'googlenet', 'shufflenet_v2_x0_5', 'shufflenet_v2_x1_0', 'shufflenet_v2_x1_5',
-#     'shufflenet_v2_x2_0', 'mobilenet_v2', 'mobilenet_v3_large', 'mobilenet_v3_small',
-#     'resnext50_32x4d', 'resnext101_32x8d', 'wide_resnet50_2', 'wide_resnet101_2',
-#     'mnasnet0_5', 'mnasnet0_75', 'mnasnet1_0', 'mnasnet1_3'
-# ]
+class FocalLoss(nn.Module):
+    def __init__(self, weight=None, gamma=2, reduction="mean"):
+        nn.Module.__init__(self)
+        self.weight = weight
+        self.gamma = gamma
+        self.reduction = reduction
 
-model_name = "shufflenet_v2_x0_5"
-Model = torchvision.models.shufflenet_v2_x0_5(pretrained=True)
-Model = Model.fc.out_features = 9
-
-# timm사용시에는 이렇게
-# Model = timm.create_model("efficientnet_b0", num_classes=9, pretrained=True)
+    def forward(self, input_tensor, target_tensor):
+        log_prob = F.log_softmax(input_tensor, dim=-1)
+        prob = torch.exp(log_prob)
+        return F.nll_loss(
+            ((1 - prob) ** self.gamma) * log_prob,
+            target_tensor,
+            weight=self.weight,
+            reduction=self.reduction,
+        )
 
 
 def train_pretrained(
     model_name: str,
-    from_pretrained: str,
-    log_name: str,
-    model_config: None,
     data_config: Dict[str, Any],
     log_dir: str,
+    log_name: str,
     fp16: bool,
     device: torch.device,
+    num_classes: int = 9,
 ) -> Tuple[float, float, float]:
     """Train."""
-    # save model_config, data_config
-    with open(os.path.join(log_dir, "data.yml"), "w") as f:
-        yaml.dump(data_config, f, default_flow_style=False)
-    with open(os.path.join(log_dir, "model.yml"), "w") as f:
-        yaml.dump(model_config, f, default_flow_style=False)
 
-    model = Model
+    img_size = data_config["IMG_SIZE"]
 
-    model_path = os.path.join(log_dir, "best.pt")
-    print(f"Model save path: {model_path}")
-    if os.path.isfile(model_path):
-        model.load_state_dict(torch.load(model_path, map_location=device))
+    # model = getattr(__import__("src.models", fromlist=[""]), model_name)()  # "Resnet34"
+    model = torchvision.models.resnet34(pretrained=True)
+    model.fc = nn.Linear(512, num_classes)
+
+    if args.prune_resnet34:
+        print("Start pruning")
+        MODEL_PATH = f"/opt/ml/input/exp/Resnet34/Resnet34_{data_name}.pt"
+        model = model_structured_prune_for_resnet34(model)
+        print("End pruning")
+        model.load_state_dict(torch.load(MODEL_PATH))
+
     model.to(device)
+    model_path = os.path.join(log_dir, f"{log_name}.pt")
+    print(f"Model save path: {model_path}")
 
     # Create dataloader
     train_dl, val_dl, test_dl = create_dataloader(data_config)
 
     # Calc macs
-    macs = calc_macs(model, (3, data_config["IMG_SIZE"], data_config["IMG_SIZE"]))
+    macs = calc_macs(model, (3, img_size, img_size))
     print(f"macs: {macs}")
 
     # Create optimizer, scheduler, criterion
     optimizer = torch.optim.SGD(
         model.parameters(), lr=data_config["INIT_LR"], momentum=0.9
     )
-    # adamp.AdamP(model.parameters(), lr=data_config["INIT_LR"], weight_decay=1e-5)
-    scheduler = torch.optim.lr_scheduler.OneCycleLR(
-        optimizer=optimizer,
-        max_lr=data_config["INIT_LR"],
-        steps_per_epoch=len(train_dl),
-        epochs=data_config["EPOCHS"],
-        pct_start=0.05,
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer=optimizer, T_max=20
     )
+    # criterion = FocalLoss()
     criterion = CustomCriterion(
         samples_per_cls=get_label_counts(data_config["DATA_PATH"])
         if data_config["DATASET"] == "TACO"
         else None,
         device=device,
     )
+
     # Amp loss scaler
     scaler = (
         torch.cuda.amp.GradScaler() if fp16 and device != torch.device("cpu") else None
@@ -100,22 +102,28 @@ def train_pretrained(
 
     # Create trainer
     trainer = TorchTrainer(
-        model_name=model_name,
         model=model,
-        model_macs=macs,
-        log_name=log_name,
         criterion=criterion,
         optimizer=optimizer,
         scheduler=scheduler,
+        macs=macs,
         scaler=scaler,
         device=device,
         model_path=model_path,
-        verbose=1,
+        wandb_name=log_name,  # log_name,
     )
-    best_acc, best_f1 = trainer.train(
+    best_lbs, best_test_acc, best_test_f1 = trainer.train(
         train_dataloader=train_dl,
         n_epoch=data_config["EPOCHS"],
         val_dataloader=val_dl if val_dl else test_dl,
+    )
+    print(
+        "best_lbs",
+        best_lbs,
+        "best_test_acc:",
+        best_test_acc,
+        "best_test_f1:",
+        best_test_f1,
     )
 
     # evaluate model with test set
@@ -130,36 +138,48 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train model.")
 
     parser.add_argument(
-        "--data", default="configs/data/taco.yaml", type=str, help="data config"
+        "--data",
+        default="/opt/ml/p4-opt-5-vibrhanium-/baseline/configs/data/taco_sample.yaml",
+        type=str,
+        help="data config",
+    )
+    parser.add_argument(
+        "--model", required=True, type=str, help="model name",
+    )
+    parser.add_argument(
+        "--prune_resnet34", default=False, type=str, help="pruning resnet34",
+    )
+    parser.add_argument(
+        "--num_classes", required=True, type=int, help="number of classes",
     )
     args = parser.parse_args()
 
-    model_config = None
+    import sys
+    import os.path as p
 
-    data_config = read_yaml(cfg=args.data)
+    ROOT_PATH = "/opt/ml/"
+    BASE_PATH = p.join(ROOT_PATH, "p4-opt-5-vibrhanium-/baseline")
+    sys.path.append(BASE_PATH)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    cur_time = datetime.now().strftime("%m%d_%H%M")
+    print("cur_time", cur_time)
 
-    try:
-        now_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        log_name = f"{model_name}_{now_str}"
-        log_dir = os.path.join(
-            "/opt/ml/input/pretrained_exp", f"{model_name}_{now_str}"
-        )
-        os.makedirs(log_dir, exist_ok=True)
+    data_config = read_yaml(cfg=args.data)
+    model_name = args.model
+    data_name = args.data.split("/")[-1][:-5]
 
-        test_loss, test_f1, test_acc = train_pretrained(
-            model_name=model_name,
-            from_pretrained="True",
-            log_name=log_name,
-            model_config=model_config,
-            data_config=data_config,
-            log_dir=log_dir,
-            fp16=data_config["FP16"],
-            device=device,
-        )
-    except NotImplementedError as e:
-        print(model_name)
-        print(e)
-    except:
-        print(f"cant do with model {model_name}")
+    log_dir = os.path.join("/opt/ml/input/exp", model_name)
+    os.makedirs(log_dir, exist_ok=True)
+    log_name = f"{model_name}_{data_name}_{data_config['EPOCHS']}_{cur_time}"
+    print("log_name", log_name)
+
+    test_loss, test_f1, test_acc = train_pretrained(
+        model_name=model_name,
+        data_config=data_config,
+        log_dir=log_dir,
+        log_name=log_name,
+        fp16=False,
+        device=device,
+        num_classes=args.num_classes,
+    )
